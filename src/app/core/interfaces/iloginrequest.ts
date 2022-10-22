b82012935af9a485c77fe3b5c12b2187951d916b
davael <dacisneros5@hotmail.com>
2022-10-22 09:57:12 -0300

    refactor: cambio login, ignore environments var

--- src/app/core/interfaces/iloginrequest.ts
+++ src/app/core/interfaces/iloginrequest.ts
@@ -1,2 +1,4 @@
 export interface Iloginrequest {
+  username: string,
+  password: string
 }
--- src/app/modules/auth/services/auth.service.ts
+++ src/app/modules/auth/services/auth.service.ts
@@ -5,6 +5,10 @@ import { Iloginrequest } from 'src/app/core/interfaces/iloginrequest';
 import { catchError, ignoreElements, tap } from 'rxjs/operators';
 import { BehaviorSubject, EMPTY } from 'rxjs';
 import { IUser } from 'src/app/core/interfaces/iuser';
+import { environment } from '../../../../environments/environment'
+
+
+const url = environment.APIUrl;
 
 @Injectable({
   providedIn: 'root'
@@ -14,13 +18,17 @@ export class AuthService {
   private user = new BehaviorSubject<IUser| any>(null);
   user$ = this.user.asObservable();
 
-  constructor(private httpClient: HttpClient, private router: Router) { }
+  constructor(private httpClient: HttpClient, private router: Router) {
+    let userlocal = localStorage.getItem('user');
+    if(userlocal)
+      this.user.next(JSON.parse(userlocal));
+   }
 
 
-  login(credentials: Iloginrequest) {
+  login(credentials: any) {
     console.log('ingresa');
     return this.httpClient
-      .post(``,credentials)
+      .post(url +`${url}login/authenticate`, credentials)
       .pipe(
         tap(token => {
           this.handleSuccesfulLogin(token);
@@ -34,7 +42,8 @@ export class AuthService {
 
   private handleSuccesfulLogin(token: any): void {
     this.user.next(token.user)
-    //this._authToken = token;
+    localStorage.setItem('token',token.token);
+    localStorage.setItem('user', JSON.stringify( token.user));
     this.redirectToHome();
   }
 
